b86eeb35d66d7c195030469cf3b04448912b5a8c
Kaverigithub <77496696+Kaverigithub@users.noreply.github.com>
2023-10-12 14:02:51 +0530

    added and made changes index.java

---
+++ index.java
@@ -0,0 +1,7 @@
+et's explain these parameters before seeing some examples:
+
+    char represents a single character in a string.
+    fromIndex signifies the position where the search for the index of a character or substring should begin. This is important where you have two characters/strings that have the same value in a string. With this parameter, you can tell the indexOf method where to start its operation from.
+    str represents a substring in a string. 
+
+Don't worry if you don't yet understand how any of this works – the examples will make it all clear!
